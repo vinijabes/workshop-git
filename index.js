3e1af6b84ea4ce0e301d5d801eba4e3038ccbdf5
Vinicius Jabes <vinijabes@gmail.com>
2018-04-07 10:58:14 -0300

    implement sum

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = class Calculator{
 		return String("a = "+a, "b = "+b);
 	}
 
-	// implementar as outras funções aqui
+	// implementar as outras funções aqui 
+  sum(a,b){
+    return a + b;
+  } 
 }
 
